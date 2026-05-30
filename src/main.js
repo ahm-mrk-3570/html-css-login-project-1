b9a6982f6bd06bd1c24d46f68cb4667a7ac14713
AHM MRK <ahmmrk.3570m@gmail.com>
2026-05-31 00:37:53 +0330

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import "./style.css";
+import person from './assets/icons8-person-100.png';
+import password from './assets/icons8-password-100.png';
+
+document.querySelector("#app").innerHTML = `
+  <div class="wrapper">
+    <form action="">
+        <h1>Login</h1>
+        <div class="input-box">
+            <input type="text" placeholder="Username" required>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+            </svg>
+        </div>
+        <div class="input-box">
+            <input type="password" placeholder="password" required>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 0a4 4 0 0 1 4 4v2.05a2.5 2.5 0 0 1 2 2.45v5a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 2 13.5v-5a2.5 2.5 0 0 1 2-2.45V4a4 4 0 0 1 4-4M4.5 7A1.5 1.5 0 0 0 3 8.5v5A1.5 1.5 0 0 0 4.5 15h7a1.5 1.5 0 0 0 1.5-1.5v-5A1.5 1.5 0 0 0 11.5 7zM8 1a3 3 0 0 0-3 3v2h6V4a3 3 0 0 0-3-3"/>
+            </svg>
+        </div>
+        <div class="remember-forget">
+            <label><input type="checkbox">Remember me</label>
+            <a href="#">Forget password</a>
+        </div>
+
+        <button type="submit" class="btn">Login</button>
+
+        <div class="register-link">
+            <p>Dont have account?
+                <a href="#">
+                Register
+                </a>
+            </p>
+        </div>
+    </form>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
